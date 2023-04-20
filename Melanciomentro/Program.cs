3a493790685dd111963bd776547af5549bc5a509
kaylp07 <kaylanelais05@gmail.com>
2023-04-20 14:29:47 -0300

    Adicionar arquivos de projeto.

---
+++ Melanciomentro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 
+    VerduraoDoJoao.Melanciomentro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Melancia Comum 2-Melancia Baby");
+            int op =
+                int.Parse(Console.ReadLine());
+            double preco;
+            if (op == 1)
+            {
+                preco = 5.50;
+                Console.WriteLine("Melancia Comum:"+ preco.ToString());
+                
+             }
+            else if (op ==2) 
+            {
+                preco = 8.56;
+                Console.WriteLine("Melancia Baby:" + preco.ToString());
+            }
+            else
+
+            {
+                Console.WriteLine("Não existe esta opção!");
+
+            }
+        }
+
+    }
+}
